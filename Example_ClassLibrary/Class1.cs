63a0aaa267d4f46c833ad05ab5ae65e6d2521ec4
StasSokol <sokol-forward-9@mail.ru>
2021-06-01 10:33:03 +0300

    init

---
+++ Example_ClassLibrary/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_ClassLibrary
+{
+    public class Class1
+    {
+        public int Num_a(int a)
+        {
+          return a;
+        }
+
+        private int Num_b(int b)
+        {
+            return b;
+        }
+
+        public int Num_c(int c)
+        {
+            return c;
+        }
+
+        protected int Num_d(int d)
+        {
+            return d;
+        }
+        
+    }
+
+    public class Class2
+    {
+        public string Str_a(string a)
+        {
+            return a;
+        }
+
+        private string Str_b(string b)
+        {
+            return b;
+        }
+
+        internal string Str_c(string c)
+        {
+            return c;
+        }
+
+        protected string Str_d(string d)
+        {
+            return d;
+        }
+
+    }
+}
---
+++ Test_work/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_work
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Path;
+        
+         public void Get_Path(string Path)
+        {
+            List<String> list_dll = new List<string>();
+            
+            
+            list_dll.AddRange(Directory.EnumerateFiles(Path, "*.*", SearchOption.AllDirectories).Where(path => path.EndsWith(".dll") || path.EndsWith(".DLL")));
+
+            foreach (var dll in list_dll)
+            {
+                try
+                {
+                    Assembly sb = Assembly.LoadFile(dll);
+
+                    foreach (var sb_type in sb.GetTypes())
+                    {
+                        listBox.Items.Add(sb_type.Name);
+                        
+                        foreach (var method in sb_type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly))
+                        {
+                            if (method.IsFamily || method.IsPublic)
+                            {
+                                listBox.Items.Add(method.Name);
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+        public MainWindow()
+        {   
+            InitializeComponent();
+          
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(Path = textBox.Text))
+            {
+                Get_Path(Path);
+        
+            }
+            else
+            {
+                MessageBox.Show("Wrong path!");
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+        }
+    }
+}
